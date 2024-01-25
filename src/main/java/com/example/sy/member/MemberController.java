c4f7482570d75236469ba44995567b4f654173cf
이승연 <tmddus2123@github.com>
2024-01-25 16:41:24 +0900

    layout

---
+++ src/main/java/com/example/sy/member/MemberController.java
@@ -0,0 +1,9 @@
+package com.example.sy.member;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+	
+	
+}
--- src/main/java/com/example/sy/page/PageController.java
+++ src/main/java/com/example/sy/page/PageController.java
@@ -1,14 +1,15 @@
 package com.example.sy.page;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 	
-	@GetMapping("/")
-	public String Home() {
+	@GetMapping("/main")
+	public String Home(Model model) {
 		
-		return "home";
+		return "main";
 	}
 }
