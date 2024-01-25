a63b515f586618ca848f9ed5a2196f574cc3a49e
이승연 <tmddus2123@gmail.com>
2024-01-25 17:41:12 +0900

    01/25 1

--- src/main/java/com/example/sy/page/PageController.java
+++ src/main/java/com/example/sy/page/PageController.java
@@ -1,14 +1,13 @@
 package com.example.sy.page;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 	
 	@GetMapping("/main")
-	public String Home(Model model) {
+	public String Home() {
 		
 		return "main";
 	}
