0d225a94fe5e99a2dbd743ffcdf80427ee224b23
이승연 <tmddus2123@gmail.com>
2024-01-30 09:41:00 +0900

    비동기 테스트

--- src/main/java/com/example/sy/member/MemberController.java
+++ src/main/java/com/example/sy/member/MemberController.java
@@ -3,6 +3,7 @@ package com.example.sy.member;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,25 @@ public class MemberController {
 	
 	@PostMapping("/member/test")
 	public Map<String, Object> testAjax(@RequestParam Map<String, Object> params) {
-
+		
+		System.out.println("params --> " + params);
+		
+		return params;
+	}
+	
+	@PostMapping("/member/testFetch")
+	public Map<String, Object> testFetch(@RequestBody Map<String, Object> params) {
+		
+		System.out.println("params --> " + params);
+		
+		return params;
+	}
+	
+	@PostMapping("/member/testXhr")
+	public Map<String, Object> testXhr(@RequestBody Map<String, Object> params) {
+		
+		System.out.println("params --> " + params);
+		
 		return params;
 	}
 }
