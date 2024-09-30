b85209eb2c8c08de81c9585feea88a4543dc4f70
tmddus2123 <tmddus2123@gmail.com>
2024-09-30 16:19:25 +0900

    1) 24.09.30

--- src/main/java/com/example/web/config/CustomRequestInterceptor.java
+++ src/main/java/com/example/web/config/CustomRequestInterceptor.java
@@ -1,10 +1,24 @@
 package com.example.web.config;
 
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Component
 public class CustomRequestInterceptor implements HandlerInterceptor {
- 	//TODO
-	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+        String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+		System.out.println("interceptor " + body);
+		// TODO Auto-generated method stub
+		request.setAttribute(body, body);
+		return true;
+	}
 }
--- src/main/java/com/example/web/config/WebMvcConfig.java
+++ src/main/java/com/example/web/config/WebMvcConfig.java
@@ -2,8 +2,10 @@ package com.example.web.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -12,11 +14,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 // lombok에서 자동으로 의존성 주입해주는 어노테이션
 public class WebMvcConfig implements WebMvcConfigurer {
-	//private final CustomArgumentResolver customArgumentResolver;
+	
+	@Autowired
+	private CustomRequestInterceptor customRequestInterceptor;
+	
 	
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		// TODO Auto-generated method stub
 		resolvers.add(new CustomArgumentResolver());
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addInterceptor(customRequestInterceptor).addPathPatterns("/**");
 	}
 }
